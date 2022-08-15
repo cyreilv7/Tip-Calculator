678a2481d00b7dae1e9e6ff5a38fb2251029d584
cyreilv7 <shoiiru3@gmail.com>
2022-08-14 23:33:38 -0700

    connect storyboard objects to viewController

--- tip-calculator/tip-calculator/ViewController.swift
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -2,18 +2,30 @@
 //  ViewController.swift
 //  tip-calculator
 //
-//  Created by Will on 8/13/22.
+//  Created by Will on 8/14/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func calculateTip(_ sender: Any) {
+    }
+    
 
 }
-

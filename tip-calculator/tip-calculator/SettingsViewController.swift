3e9c86160ede33a0bf6b30ccb48e27307749feb4
cyreilv7 <shoiiru3@gmail.com>
2022-08-15 03:15:58 -0700

    store user tip percentages in defaults

--- tip-calculator/tip-calculator/SettingsViewController.swift
+++ tip-calculator/tip-calculator/SettingsViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var newTip1: UITextField!
+    
+    @IBOutlet weak var newTip2: UITextField!
+    
+    @IBOutlet weak var newTip3: UITextField!
+    
+    
+    @IBAction func updateTips(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        
+        defaults.set(newTip1.text, forKey: "tip1")
+        defaults.set(newTip2.text, forKey: "tip2")
+        defaults.set(newTip3.text, forKey: "tip3")
+        
+        defaults.synchronize()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    
 }
--- tip-calculator/tip-calculator/TipViewController.swift
+++ tip-calculator/tip-calculator/TipViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController {
 
     @IBOutlet weak var billAmountTextField: UITextField!
-    
     @IBOutlet weak var tipAmountLabel: UILabel!
-    
     @IBOutlet weak var tipControl: UISegmentedControl!
-    
     @IBOutlet weak var totalLabel: UILabel!
+    
+    var tipPercentages: [Double] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = UserDefaults.standard
+        
+        let tip1 = Double(defaults.string(forKey: "tip1") ?? "15")
+        let tip2 = Double(defaults.string(forKey: "tip2") ?? "18")
+        let tip3 = Double(defaults.string(forKey: "tip3") ?? "20")
+
+        print(tip1, tip2, tip3)
+        tipPercentages = [tip1!, tip2!, tip3!]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +37,7 @@ class ViewController: UIViewController {
     
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
-        
-        let tipPercentages = [0.15, 0.18, 0.2]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex] / 100
         let total = tip + bill
         
         tipAmountLabel.text = String(format: "$%.2f", tip)

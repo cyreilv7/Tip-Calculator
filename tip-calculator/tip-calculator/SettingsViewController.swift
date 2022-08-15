0f847cc90c71138afc56111af58a3bad4d433697
cyreilv7 <shoiiru3@gmail.com>
2022-08-15 01:16:20 -0700

    add View Controller for settings

---
+++ tip-calculator/tip-calculator/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  tip-calculator
+//
+//  Created by Will on 8/15/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
